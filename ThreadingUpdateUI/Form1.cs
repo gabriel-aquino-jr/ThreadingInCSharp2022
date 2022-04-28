e50304a1aed22eb92f5f413be88cdf817984b4ac
galzer <galzer@outlook.com>
2022-04-28 09:24:49 -0300

    Added multi-threading implementation for GO button

--- ThreadingUpdateUI/Form1.cs
+++ ThreadingUpdateUI/Form1.cs
@@ -18,10 +18,41 @@ namespace ThreadingUpdateUI
             InitializeComponent();
         }
 
+        List<Label> threadLabels = new List<Label>();
       
         private void btnGo_Click(object sender, EventArgs e)
         {
+            if (threadLabels.Count < 10)
+            {
+                int.TryParse(textBox1.Text, out int i);
+                int.TryParse(textBox2.Text, out int sleep);
 
+                Label l = new Label();
+                l.Text = DateTime.Now.ToLongTimeString();
+
+                threadLabels.Add(l);
+
+                l.Top = (panel1.Controls.Count) * l.Height + 20;
+                l.Left = 5;
+
+                this.panel1.Controls.Add(l);
+
+                //btnGoNoThread.Enabled = textBox1.Enabled = textBox2.Enabled = false;
+
+                Task.Factory.StartNew(() =>
+                   {
+                   for (int j = 0; j <= i; j++)
+                   {
+                           // to avoid exception of using thread
+                           this.Invoke((MethodInvoker)delegate
+                           {
+                               l.Text = j.ToString();
+                           }
+                           );                           
+                           System.Threading.Thread.Sleep(sleep);
+                       }
+                   });
+            }
         }
 
         private void btnGoNoThread_Click(object sender, EventArgs e)
@@ -34,6 +65,7 @@ namespace ThreadingUpdateUI
             for (int j = 0; j <= i; j++)
             {
                 lblCounter.Text = j.ToString();
+                lblCounter.Refresh();
                 System.Threading.Thread.Sleep(sleep);
             }
 
@@ -45,7 +77,7 @@ namespace ThreadingUpdateUI
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             //We need to kill any running threads
-            
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
         }
     }
 }
